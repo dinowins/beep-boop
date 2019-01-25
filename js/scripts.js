ff3297061b36e45414438fb927b22a09fc58bff0
Dino Nguyen <dinodnguyen@gmail.com>
2019-01-25 14:31:30 -0800

    Add CSS, HTML, jQuery elements for aesthetics

--- js/scripts.js
+++ js/scripts.js
@@ -1,58 +1,39 @@
 //Business Logic
 
-//Spec 1
-
-// function returnRange(num) {
-//   var range = [];
-//   for (var i = 0; i < num+1; i++) {
-//     range.push(" " + i + " ");
-//     // console.log(i);
-//   }
-//   return range;
-// }
-
-//Business Logic
-
 function returnRange(num) {
   var range = [];
   for (var i = 0; i < num+1; i++) {
     range.push(i);
-    // console.log(range[i]);
   }
   return range;
 }
 
 function beepBoop(array) {
   var beepArray = [];
-  var sorryDave = "I\'m sorry, Dave. I\'m afraid I can\'t do that";
+  var sorryDave = " I\'m sorry, Dave. I\'m afraid I can\'t do that. ";
   for (var i = 0; i < array.length; i++) {
     var stringOfI = array[i].toString();
     var splitString = stringOfI.split('');
     if (splitString.includes('3')) {
       beepArray.push(sorryDave);
     } else if (splitString.includes('2')) {
-      beepArray.push("Boop");
-      // console.log("Boop");
+      beepArray.push(" Boop ");
     } else if (splitString.includes('1')) {
-      beepArray.push("Beep");
-      // console.log("Beep");
+      beepArray.push(" Beep ");
     } else {
-      beepArray.push(array[i]);
+      beepArray.push(" " + array[i] + " ");
     }
-    // console.log(stringOfI);
-    // console.log(splitString);
   }
-  console.log(beepArray);
+  // console.log(beepArray);
   return beepArray;
 }
-
 
 //UI Logic
 
 $(function(){
   $("button").click(function() {
     var result = beepBoop(returnRange((parseInt($("#userNumber").val()))));
-    // console.log(result);
+    $(".well").show();
     $("#result").text(result);
   });
 });
